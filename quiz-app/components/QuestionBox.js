e7c499b0724052b0775ccac19efe017adb10a0ff
Alireza Hasanzade <alirezawtf321@gmail.com>
2023-08-19 08:37:56 -0700

    fix bug

--- quiz-app/components/QuestionBox.js
+++ quiz-app/components/QuestionBox.js
@@ -5,19 +5,16 @@ import { useEffect, useState } from "react";
 const QuestionBox = ({ questions, handlScore, handleEndOfQuiz }) => {
   const [index, setIndex] = useState(0);
   const [correct, setCorrect] = useState();
-  const [colorIndex, setColorIndex] = useState(5);
+  const [colorIndex, setColorIndex] = useState();
   console.log(colorIndex);
-  useEffect(() => {
-    setColorIndex("");
-  }, []);
+
   const handleAnswer = (e, answer, i) => {
-    if (answer) {
+    if (answer == true) {
       handlScore();
       setCorrect(true);
     }
     setColorIndex(i);
     setTimeout(() => {
-      handlScore(index);
       if (index < questions.length - 1) {
         setIndex(index + 1);
       }
@@ -31,7 +28,7 @@ const QuestionBox = ({ questions, handlScore, handleEndOfQuiz }) => {
   return (
     <div className="w-full md:w-[60%] md:py-[56px] md:mx-auto flex flex-col md:flex-row px-4 md:px-1 py-5 bg-yellow-400 rounded-lg">
       <div className="md:w-1/2  md:gap-10 p-4 flex flex-col gap-2">
-        <p className="w-full text-4xl font-bold text-center md:text-left md:text-6xl">
+        <p className="w-full md:text-3xl text-4xl font-bold text-center md:text-left lg:text-6xl">
           Question {index + 1}/{questions.length}
         </p>
         <p className="text-xl font-medium md:text-2xl">
@@ -46,9 +43,8 @@ const QuestionBox = ({ questions, handlScore, handleEndOfQuiz }) => {
               onClick={(e) => handleAnswer(e, item.isCorrect, i)}
               className={clsx(
                 "border-2 border-black p-2 rounded-xl cursor-pointer bg-yellow-400",
-                { "bg-green-500": item.isCorrect && colorIndex === i },
-                { "bg-red-500": !item.isCorrect && colorIndex === i },
-                { "bg-yellow-400": colorIndex === "" }
+                { "bg-green-700": item.isCorrect && colorIndex === i },
+                { "bg-red-700": !item.isCorrect && colorIndex === i }
               )}
             >
               {item.answerText}
